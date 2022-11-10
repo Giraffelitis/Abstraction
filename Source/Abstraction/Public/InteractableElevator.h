be19c13b635ac22f60c7078181d817748b370003
Giraffelitis <71671883+Giraffelitis@users.noreply.github.com>
2022-11-10 13:12:53 -0800

    Added Levels & Elevator Base code
    Levels and elevator still need to be built out but code is there ready to be implemented.

---
+++ Source/Abstraction/Public/InteractableElevator.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/StaticMeshActor.h"
+#include "InteractableElevator.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ABSTRACTION_API AInteractableElevator : public AStaticMeshActor
+{
+	GENERATED_BODY()
+	
+};
